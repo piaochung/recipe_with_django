cd204d39ad520a0ad584febcafb5023799851b8c
chungchung <hangjastar@gmail.com>
2021-07-07 19:54:58 +0900

    Add User Account

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register(request):
+    pass
+
+def login(request):
+    pass
+
+def logout(request):
+    pass
--- righteous/settings.py
+++ righteous/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'store',
     'categories',
+    'accounts',
     'rest_framework',
 ]
 
